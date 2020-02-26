d47a007b5b8dad9da066459cfc3a0fb152ca0a35
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-26 15:07:13 +0530

    Added: A method to reset the board has been added.

--- ticTacToe.java
+++ ticTacToe.java
@@ -1,5 +1,18 @@
 public class ticTacToe {
 
+    public static void boardReset(int[][] game) {
+        int ROW=3;
+        int COLUMN=3;
+        for(int index1=0; index1<ROW; index1++)
+        {
+            for(int index2=0; index2<COLUMN; index2++)
+            {
+                game[index1][index2]=1;
+                System.out.print(game[index1][index2]);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To TicTacToe Game.");
     }
